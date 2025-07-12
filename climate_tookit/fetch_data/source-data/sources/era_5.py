5fb0cb8cdbe14081f33046c840ac5e5d0b9f1213
John Mwangi <mail.mwangi@gmail.com>
2025-07-12 13:30:35 +0300

    chore: Remove unused code

--- climate_tookit/fetch_data/source-data/sources/era_5.py
+++ climate_tookit/fetch_data/source-data/sources/era_5.py
@@ -2,13 +2,11 @@ import logging
 import os
 from datetime import date
 
-import pandas as pd
 from cdsapi.api import Client
 from dotenv import load_dotenv
 
 from .utils import models
 from .utils.settings import Settings, set_logging
-from .utils.utils import get_gee_data_daily, get_gee_data_monthly
 
 load_dotenv()
 
@@ -65,44 +63,5 @@ class DownloadData(models.DataDownloadBase):
     def download_soil_moisture(self):
         raise NotImplementedError
 
-    def download_variables(self) -> pd.DataFrame:
-
-        settings = self.settings
-        source = self.source
-        variables = self.variables
-        data_settings = getattr(settings, source.name)
-
-        func = (
-            get_gee_data_monthly
-            if data_settings.cadence == models.Cadence.monthly.name
-            else get_gee_data_daily
-        )
-
-        climate_data = func(
-            image_name=data_settings.gee_image,
-            location_coord=self.location_coord,
-            from_date=self.date_from_utc,
-            to_date=self.date_to_utc,
-            scale=data_settings.resolution,
-        )
-
-        dataset_cols = list(climate_data.columns)
-        req_vars = [v.name for v in variables]
-
-        available_cols = []
-        missing_vars = []
-        for v in variables:
-            try:
-                c = getattr(data_settings.variable, v.name)
-                available_cols.append(c)
-            except:
-                logger.warning(
-                    f"{source.name.upper()} does not have {v.name} data"
-                )
-                missing_vars.append(v.name)
-
-        logger.info(f"Available columns: {dataset_cols}")
-        logger.info(f"Requested variables: {req_vars}")
-
-        cols = ["date"] + available_cols
-        return climate_data[cols]
+    def download_variables(self):
+        raise NotImplementedError
