430158cacf1ce3d1bd1206365af4d90bd65496c8
sammyjoseph999m <sammyjoseph999@gmail.com>
2025-09-10 16:37:06 +0300

    chore: update module export

--- climate_tookit/fetch_data/transform_data/__init__.py
+++ climate_tookit/fetch_data/transform_data/__init__.py
@@ -1,4 +1,3 @@
 from .transform_data import transform_data
-from .data_dictionary import SOURCE_VARIABLE_MAPPINGS
  
-__all__ = ['transform_data', 'SOURCE_VARIABLE_MAPPINGS']
+__all__ = ['transform_data']
